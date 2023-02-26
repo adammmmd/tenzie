800d283d6705d2a77ff049e9d4ee364f0dcb1eab
adammmmd <adam.muhammad.onduty@gmail.com>
2023-02-26 21:13:28 +0700

    adding dice img

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 
 export default function Die(props) {
+
+    const [diceValue, setdiceValue] = React.useState([props.value])
+
+    const dice = diceValue.map(die => (
+        <img 
+            className="dice"
+            src={`./Dice-${die}.svg`} 
+        />
+    ))
+
+
     return (
         <div 
             className={`number ${props.isHeld && "held"}`}
             onClick={props.hold}
         >
-            <h1>{props.value}</h1>
+            {dice}
         </div>
     )
 }
